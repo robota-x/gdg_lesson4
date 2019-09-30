7925f0e7d6f1f6ba27d3f942911c7f3b689ee3db
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-09-30 23:06:51 +0100

    send over html file

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ This is a repo going over the steps taken during the fourth lessons of the Londo
     - npm init and install express
     - setup basic listening server on `server.js`
     - install and setup nodemon for easy reload with `npm start` script
+
+* hello world
+    - send a string as a response
+    - inspect the cookie-cutter html file and remember the js script
+    - send the file instead
--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send("hello there");
+  response.sendFile(__dirname + "/index.html");
+
+  /* this is way faster than doing
+  const fs = require("fs");
+  fs.readFile("./index.html", "utf-8", (err, data) => response.send(data));
+  */
 });
 
 app.listen(3000, () => console.log("server listening on port 3000"));
