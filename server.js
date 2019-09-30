8d7de5d89d74e6d862708316ed7278b06f8cf2a7
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-09-30 22:56:47 +0100

    add basic hello route

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("hello there");
+});
+
 app.listen(3000, () => console.log("server listening on port 3000"));
